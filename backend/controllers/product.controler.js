89d7fe9224ae10b74d09db347d892bc13e7601f2
Eng Mahmoud Majed <mahmoudmajed404@gmail.com>
2026-08-20 00:55:21 +0300

    finish products route

--- backend/controllers/product.controler.js
+++ backend/controllers/product.controler.js
@@ -3,47 +3,48 @@ import Product from "../models/product.model.js";
 import cloudinary from "../lib/cloudinary.js";
 
 export const getAllProducts = async (req, res) => {
-    try {
-        const products = await Product.find({});
-        res.status(200).json({products});
-    } catch (error) {
-        res.status(500).json({ message: error.message });
-    }
+  try {
+    const products = await Product.find({});
+    res.status(200).json({ products });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 };
 
 export const getFeaturedProducts = async (req, res) => {
-    // .lean() method is used to convert the Mongoose documents into plain JavaScript objects.
-    try {
-        let featuredProducts = await redis.get("featured_products");
-        if (!featuredProducts) {
-            featuredProducts = await Product.find({ isFeatured: true }).lean();
-            await redis.set("featured_products", JSON.stringify(featuredProducts), "EX", 3600);
-        } else {
-            featuredProducts = JSON.parse(featuredProducts);
-        }
-        res.status(200).json({ featuredProducts }); 
-    } catch (error) {
-        res.status(500).json({ message: error.message });
+  try {
+    let featuredProducts = await redis.get("featured_products");
+    if (!featuredProducts) {
+      featuredProducts = await Product.find({ isFeatured: true }).lean();
+      await redis.set("featured_products", JSON.stringify(featuredProducts), { ex: 3600 });
+    } else {
+      featuredProducts = typeof featuredProducts === "string"
+        ? JSON.parse(featuredProducts)
+        : featuredProducts;
     }
+    res.status(200).json({ featuredProducts });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 };
 
 
 export const createProduct = async (req, res) => {
-    try {
-        const { name, description, price, isFeatured, category ,imageUrl} = req.body;
-        const newProduct = new Product({ name, description, price, isFeatured, category, imageUrl });
-        let cloudnaryResponse = await cloudinary.uploader.upload(imageUrl, {
-            folder: "products",
-            width: 500,
-            height: 500,
-            crop: "fill"
-        });
-        newProduct.imageUrl = cloudnaryResponse.secure_url;
-        await newProduct.save();
-        res.status(201).json({ message: "Product created successfully", product: newProduct });
-    } catch (error) {
-        res.status(500).json({ message: error.message });
-    }
+  try {
+    const { name, description, price, isFeatured, category, imageUrl } = req.body;
+    const newProduct = new Product({ name, description, price, isFeatured, category, imageUrl });
+    let cloudnaryResponse = await cloudinary.uploader.upload(imageUrl, {
+      folder: "products",
+      width: 500,
+      height: 500,
+      crop: "fill"
+    });
+    newProduct.imageUrl = cloudnaryResponse.secure_url;
+    await newProduct.save();
+    res.status(201).json({ message: "Product created successfully", product: newProduct });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 };
 
 
@@ -85,3 +86,80 @@ export const deleteProduct = async (req, res) => {
     });
   }
 };
+
+
+export const getRecommendedProducts = async (req, res) => {
+  try {
+    const products = await Product.aggregate([
+      {
+        $sample: { size: 3 }
+      },
+      {
+        $project: {
+          _id: 1,
+          name: 1,
+          image: 1,
+          price: 1
+        }
+      }
+
+    ])
+    res.json(products)
+  } catch (error) {
+    console.log("Error in Recommendation", error.message);
+    res.status(500).json({ message: "Server Error", error: error.message })
+  }
+}
+
+
+export const getProductsByCategory = async (req, res) => {
+  const { category } = req.params;
+  try {
+    const products = await Product.find({ category })
+    res.json(products)
+  } catch (error) {
+    console.log("Error in getProductsByCategory controller", error.message);
+    res.status.json({ message: "Server error", error: error.message });
+  }
+}
+
+
+
+
+export const toggleFeaturedProduct = async (req, res) => {
+  try {
+
+    // Delete the product from the database
+    const product = await Product.findById(req.params.id);
+    if (product) {
+      product.isFeatured = !product.isFeatured;
+      const updatedProduct = await product.save();
+      // update cache
+      await updateFeatureProductCache();
+      res.json(updatedProduct);
+    }else{
+      res.status(404).json({message:"Product not found"})
+    }
+
+  } catch (error) {
+    console.error("error in toggle product", error.message);
+    return res.status(500).json({
+      message: "Internal Server Error",
+      error: error.message,
+    });
+  }
+};
+
+
+async function updateFeatureProductCache() {
+  try {
+    const featuredProduct = await Product.find({isFeatured:true}).lean();
+    await redis.set("featured_products",json.stringify(featuredProduct));
+  } catch (error) {
+    console.log("error in update cache function")
+  }
+}
+
+
+
+
--- backend/routes/product.route.js
+++ backend/routes/product.route.js
@@ -1,12 +1,23 @@
 import express from "express"
 const router = express.Router();
-import {getAllProducts,getFeaturedProducts,createProduct,deleteProduct} from "../controllers/product.controler.js"
+import {
+  getAllProducts,
+  getFeaturedProducts,
+  createProduct,
+  deleteProduct,
+  getRecommendedProducts,
+  getProductsByCategory,
+  toggleFeaturedProduct
+} from "../controllers/product.controler.js"
 import {protectRoute,adminRoute} from "../middlewares/auth.middleware.js"
 
-router.get("/",protectRoute,adminRoute,getAllProducts)
-router.get("/featured",getFeaturedProducts)
+router.get("/",protectRoute,adminRoute,getAllProducts);
+router.get("/featured",getFeaturedProducts);
+router.get("/recommendations",getRecommendedProducts);
+router.get("/category/:category",getProductsByCategory);
 router.post("/",protectRoute,adminRoute,createProduct);
-router.delete("/:id",protectRoute,adminRoute,deleteProduct)
+router.patch("/:id",protectRoute,adminRoute,toggleFeaturedProduct);
+router.delete("/:id",protectRoute,adminRoute,deleteProduct);
 // router.get("/:id",protectRoute,adminRoute,getAllProducts)
 // router.put("/:id",protectRoute,adminRoute,getAllProducts)
 
